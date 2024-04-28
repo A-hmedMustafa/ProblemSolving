dd009fdea254f3e51778e2d67e0c95b5208fcfdd
A-hmedMustafa <ahmedmost205@gmail.com>
2024-04-28 17:44:40 +0300

    TwoSum

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (nums[j] + nums[i] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                    else
+                    {
+                        return new int[0];
+                    }
+                }
+
+
+            }
+            return new int[0];
         }
     }
 }
